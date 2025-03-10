18d043272426b440a26a2d5628cbc8fa47d80c63
Saquib Anjum <saquibanjum8969@gmail.com>
2025-03-10 16:16:48 +0530

    __

--- FrontEnd/src/components/Hero.jsx
+++ FrontEnd/src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
           slidesToShow={1}
           slidesToScroll={1}
           autoplay={true}
-          autoplaySpeed={3000}
+          autoplaySpeed={2000}
           arrows={true}
           className="w-full"
         >
